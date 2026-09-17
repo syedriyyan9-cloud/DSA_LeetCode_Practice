b945c4ca7b73ac5ea364f8fa6dfff5cbeafe8290
Syed Riyyan Hassan <syedriyyan9@gmail.com>
2026-09-17 16:57:44 +0500

    Time: 0 ms (100%), Space: 19.3 MB (59.17%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -3,24 +3,15 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import math
 class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # traverse the list
-        count = 0
-        h1 = head
-        while head is not None:
-            count += 1
-            head = head.next
-        if count % 2 == 0:
-            count /= 2
-            count = math.ceil(count)
-            count += 1
-        else:
-            count /= 2
-            count = math.ceil(count)
-        while h1 is not None and (count - 1) != 0:
-            h1 = h1.next
-            count -= 1
-        return h1
-        
+    def middleNode(self, head: ListNode | None) -> ListNode | None:
+        s = head
+        f = head
+        output = []
+
+        while s != None:
+            if f is None or f.next is None:
+                return s
+            else:
+                f = f.next.next
+            s = s.next
