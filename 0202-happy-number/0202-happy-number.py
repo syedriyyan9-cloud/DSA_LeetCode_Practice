d0cd0c65adc99f18d8ebb9fcaa49aa82c2291321
Syed Riyyan Hassan <syedriyyan9@gmail.com>
2026-09-18 15:21:50 +0500

    Time: 12 ms (3.2%), Space: 19.3 MB (62.65%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        summ = n
+        tries = 0
+        while summ != 1 and tries != 100:
+            digits = str(summ)
+            square = 0
+            for digit in digits:
+                square += int(digit) ** 2
+            summ = square
+            tries += 1
+        if summ == 1:
+            return True
+        else:
+            return False
