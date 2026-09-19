461cdad68e1ed458f7e9ed57cf2bae24425993d8
Syed Riyyan Hassan <syedriyyan9@gmail.com>
2026-09-19 15:38:27 +0500

    Time: 31 ms (66.7%), Space: 53.7 MB (8.93%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,16 +4,12 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        value_list = []
-        while head is not None:
-            value_list.append(head.val)
-            head = head.next
-
-        l, r = 0 , len(value_list) - 1
-        while l < r:
-            if value_list[l] != value_list[r]:
-                return False
-            l += 1
-            r -= 1
-        return True
+    def isPalindrome(self, head: ListNode | None) -> bool:
+        cur = head
+        values = []
+        while cur != None:
+            values.append(cur.val)
+            cur = cur.next
+        dup = values[:]
+        dup.reverse()
+        return values == dup
